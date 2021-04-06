c1cc44e7c45fe5ffe18265df6176439d95e456e1
mustlammas <marko.kurm@gmail.com>
2021-04-06 17:11:09 +0200

    Add SERVER config parameter to webpack production config.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -20,6 +20,7 @@ module.exports = merge(common, {
       filename: '[name].[contenthash].css'
     }),
     new webpack.DefinePlugin({
+      SERVER: "'http://34.90.238.42:2222'",
       WS_SERVER: "'ws://34.90.238.42:2222/ws'"
     })
   ]
