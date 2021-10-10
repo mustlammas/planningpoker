f6dad9359b93a1b9a71f049b5dd37d02f6d4f30e
marko.kurm@gmail.com <marko.kurm@basefarm.com>
2021-10-10 11:15:08 +0300

    Use fullhash instead of the deprecated hash

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -33,7 +33,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: '[name].[hash].js',
+    filename: '[name].[fullhash].js',
     publicPath: '/'
   }
 };
